ff8dc794000a20125c19bb68fedcfebdb9b5c417
Kyle Swanson <swansonk.14@gmail.com>
2018-02-10 10:03:37 -0500

    Changing directions and colors with maps

--- fleas.py
+++ fleas.py
@@ -1,12 +1,20 @@
 import argparse
 import pygame
 
-BLACK = (0, 0, 0)
-GREY = (100, 100, 100)
-WHITE = (255, 255, 255)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
+COLORS = {
+    'black': (0, 0, 0),
+    'white': (255, 255, 255),
+    'grey': (100, 100, 100),
+    'red': (255, 0, 0),
+    'green': (0, 255, 0),
+    'blue': (0, 0, 255)
+}
+DIRECTIONS = {
+    'up': (0, 1),
+    'right': (1, 0),
+    'down': (0, -1),
+    'left': (-1, 0)
+}
 
 def column_to_pixel(col_num):
     return col_num * WIDTH + 10
@@ -18,16 +26,9 @@ def column_row_to_pixels(row_num, col_num):
     return (column_to_pixel(col_num), row_to_pixel(row_num))
 
 class Flea(pygame.sprite.Sprite):
-    up = (0, 1)
-    down = (0, -1)
-    left = (-1, 0)
-    right = (1, 0)
-    direction_map = {
-        'up': up,
-        'right': right,
-        'down': down,
-        'left': left
-    }
+    directions = ['up', 'right', 'down', 'left']
+    right_direction = {directions[i]: directions[(i+1) % len(directions)] for i in range(len(directions))}
+    left_direction = {directions[i]: directions[(i-1) % len(directions)] for i in range(len(directions))}
 
     def __init__(self, board, row, col, direction='up'):
         super(Flea, self).__init__()
@@ -35,34 +36,18 @@ class Flea(pygame.sprite.Sprite):
         self.row = row
         self.col = col
         self.rect = board.get_square(self.row, self.col).rect
-        self.direction = self.direction_map[direction]
+        self.direction = direction
         self.set_pic()
 
     def rotate_left(self):
         self.image = pygame.transform.rotate(self.image, 90)
-
-        if self.direction == self.up:
-            self.direction = self.left
-        elif self.direction == self.left:
-            self.direction = self.down
-        elif self.direction == self.down:
-            self.direction = self.right
-        else:
-            self.direction = self.up
+        self.direction = self.left_direction[self.direction]
 
     def rotate_right(self):
         self.image = pygame.transform.rotate(self.image, -90)
+        self.direction = self.right_direction[self.direction]
 
-        if self.direction == self.up:
-            self.direction = self.right
-        elif self.direction == self.right:
-            self.direction = self.down
-        elif self.direction == self.down:
-            self.direction = self.left
-        else:
-            self.direction = self.up
-
-    def step_forward(self):
+    def step(self):
         self.row = (self.row + self.direction[0]) % board.num_rows
         self.col = (self.col + self.direction[1]) % board.num_cols
         self.rect = board.get_square(self.row, self.col).rect
@@ -78,6 +63,9 @@ class Flea(pygame.sprite.Sprite):
             self.image = pygame.transform.rotate(self.image, 90)
 
 class Square(pygame.sprite.Sprite):
+    colors = ['white', 'black' 'red', 'green', 'blue']
+    next_color = {colors[i]: colors[(i+1) % len(colors)] for i in range(len(colors))}
+
     def __init__(self, row, col, color):
         super(Square, self).__init__()
         self.row = row
@@ -85,10 +73,13 @@ class Square(pygame.sprite.Sprite):
         self.color = color
 
         self.image = pygame.Surface([WIDTH, HEIGHT])
-        self.image.fill(color)
+        self.image.fill(COLORS[self.color])
         self.rect = self.image.get_rect()
         self.rect.x = column_to_pixel(self.col)
         self.rect.y = row_to_pixel(self.row)
+
+    def change_color(self):
+        self.color = self.next_color[self.color]
 
 class Board:
     def __init__(self, num_rows, num_cols):
